e7aa94cf4b95ece3b87634322fb3216e14cb7ca0
oscargon12 <oscargon12@gmail.com>
2021-03-23 00:18:10 -0500

    Ejercicio 5

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -3,17 +3,27 @@
 */
 
 console.log('=== Ejercicio 1 ===')
-const charCounter = () => {
-  let cadena = 'Hello world';
+const charCounter = (str) => {
+  let cadena = str;
   
-  if (typeof cadena == 'string'){
+  if (typeof cadena === 'string' && cadena.length > 0){
     console.log(`El valor de cadena es ${cadena}`);
     console.log(`la cantidad de caracteres es ${cadena.length}`);
-  }else {
-    console.log('El tipo de dato No es un string')
+  } else {
+    console.log('El tipo de dato No es un string, o no ingresaste uno')
   }
 }
-charCounter();
+charCounter('Holaaaa');
+
+// Ejercicio 1 profe
+console.log('=== Ejercicio 1 profe ===')
+
+const contarCaracteres = ( cadena = "" ) => // Le doy un valor vacío por defecto a cadena
+(!cadena) // = Si cadena es vacía
+?console.warn('No ingresaste una cadena') //Con operador ternario establezco las opciones
+: console.info(`la cadena tiene ${cadena.length} caracteres`)
+
+contarCaracteres('Hola que hace');
 
 // === Ejercicio 2 ===
 /* 2) Programa una función que te devuelva el texto recortado según el número
@@ -66,4 +76,42 @@ const wordRepeat = (str, n) => {
 
 }
 
-wordRepeat('Ciclo repetido || ', 10);
+wordRepeat('Ciclo repetido || ', 3);
+
+
+// === Ejercicio 5 ===
+console.log('=== Ejercicio 5 ===')
+/* 5) Programa una función que invierta las palabras de una cadena de texto, 
+pe. miFuncion("Hola Mundo") devolverá "odnuM aloH". */
+
+const stringInvert = (str) => {
+  let cadena3 = str;
+
+
+  if (typeof cadena3 === 'string' && cadena3.length > 0){
+    let cadenaSeparada = cadena3.split('');
+    let cadenaRevertida = cadenaSeparada.reverse();
+    console.log(cadenaRevertida);
+  } else{
+      console.log('No ingresaste una cadena');
+  }
+
+  /*   (typeof cadena3 === 'string' && cadena3.length > 0)
+  ?console.log(let cadenaRevertida = cadena3.reverse())
+  :console.warn('No sirve')
+
+  console.log(cadenaRevertida) */
+
+}
+
+stringInvert('Holaaaa');
+
+
+
+/* 6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2. */
+
+
+/* 7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true. */
+
+
+/* 8) Programa una función que elimine cierto patrón de caracteres de un texto dado, pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5. */
