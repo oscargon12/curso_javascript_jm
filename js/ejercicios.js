14dc98f57100f4452d549eb315672a887deba768
oscargon12 <oscargon12@gmail.com>
2021-04-13 18:44:47 -0500

    Ejercicio 26

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -593,7 +593,24 @@ const powArray = (nums = undefined) => {
 //powArray('Hola');
 powArray(['e', 'w', 'f']);
 powArray([1, 4, 5]);
+
+
+/* Solución profe */
+const devolverCuadrados = (arr = undefined) => {
+   if (arr === undefined) return console.warn('No has ingresado nada');
+   if (!(arr instanceof Array)) return console.warn('El valor ingresado no es un array');
+   if (arr.length === 0) return console.warn('El arreglo está vacío');
+
+   for (let num of arr){
+     if (typeof num !== 'number') return console.warn(`El valor ${num} No es un numero`);
+   }
+
+   const newArr = arr.map(el => el * el);
+
+   return console.log(`Arreglo original ${arr}, \nArreglo al cuadrado: ${newArr}`);
+}
  
+devolverCuadrados([1,4,8]);
 /* 22) Programa una función que dado un array devuelva el número mas alto 
 y el más bajo de dicho array, 
 pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60]. */
@@ -609,6 +626,7 @@ const highLow = (arrayNums) =>{
   let max = Math.max(...numsHL);
   let min = Math.min(...numsHL);
 
+  console.log(`umero mayor es: ${max}, numero menor: ${min}`)
   let highLow = [max, min];
   console.log(highLow);
   
@@ -626,32 +644,154 @@ pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0],
 impares: [1,3,5,7,9]}. */
 console.log('=== Ejercicio 23 ===');
 
-const pairOdd = (allNums) =>{
+const pairOdd = (allNums = undefined) =>{
 
-  let numsAll = allNums;
+  let array = allNums,
+   pares = [],
+   impares = [];
 
-  numsAll.forEach(function (el, index) {
-    let pairOddArr = el % 2;
-
-    if(el % 2 === 0){
-      let pairNums = [el];
-      let pairNumsArr = [...pairNums];
-      
-      console.log(pairNumsArr);
-    } else if (el % 2 === 1){
-      let oddNums = [el];
-      let oddNumsArr = [...oddNums];
+   for(let i = 0; i < array.length; i++){
+     if(i % 2 === 0){
+        impares.push(array[i]);
+    } else {
+      pares.push(array[i]);
     }
+   }
 
+console.log(pares);
+console.log(impares);
 
-    
-})
-
-
-  console.log(numsAll);
-
+const numsObject = {
+  pares,
+  impares,
 }
 
-pairOdd([1,2,3,4,5,6,7,8,9,0]);
+console.log(numsObject)
+}
+
+pairOdd([1,2,3,4,5,6,7,8,9,10]);
 
 
+/* Solución profe */
+
+const separarParesImpares = (arr = undefined) => {
+  if (arr === undefined) return console.warn('No has ingresado nada');
+   if (!(arr instanceof Array)) return console.warn('El valor ingresado no es un array');
+   if (arr.length === 0) return console.warn('El arreglo está vacío');
+
+   for (let num of arr){
+     if (typeof num !== 'number') return console.warn(`El valor ${num} No es un numero`);
+   }
+
+  return console.log({
+    pares: arr.filter(num => num % 2 === 0),
+    impares: arr.filter(num => num % 2 === 1)
+  })
+}
+
+separarParesImpares([1,2,3,4,5,6,7,8,9,10]);
+
+
+/* 24) Programa una función que dado un arreglo de números devuelva un objeto con dos arreglos, 
+el primero tendrá los numeros ordenados en forma ascendente y el segundo de forma descendiente, 
+pe. miFuncion([7, 5,7,8,6]) devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }. */
+console.clear()
+console.log('=== Ejercicio 24 ===');
+
+const inverse = (arr = undefined) =>{
+
+  let numsArray = arr;
+  const minMax = numsArray.sort(function(a, b){return a - b});
+  console.log(minMax)
+  
+  let numsObject = {
+    minMax: numsArray.sort(function(a, b){return a - b}),
+    maxMin: numsArray.sort(function(a, b){return a - b}).reverse()
+  }
+
+  return console.log(numsObject)
+}
+inverse([7,5,7,8,6])
+
+/* Solucion Profe */
+const ordenarArreglo = (arr = undefined) => {
+  return console.log({
+    arr, // Objeto literal
+    asc: arr.map(el => el).sort(),
+    // del arreglo arr, uso el .map para crear uno nuevo. (el => el) por cada elemento devuelve uno para el nuevo array
+    desc: arr.map(el => el).sort().reverse(),
+  });
+}
+
+ordenarArreglo([7,5,7,8,6])
+
+/* 25) Programa una función que dado un arreglo de elementos, elimine los duplicados, 
+pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true]) devolverá ["x", 10, 2, "10", true]. */
+console.log('=== Ejercicio 25 ===');
+
+const repeatNums = (arr = undefined) => {
+
+  if (arr === undefined) return console.warn('No has ingresado nada');
+   if (!(arr instanceof Array)) return console.warn('El valor ingresado no es un array');
+   if (arr.length === 0) return console.warn('El arreglo está vacío');
+  if  (arr.length === 1) return console.warn('El arreglo debe tener almenos 2 elementos');
+
+let numsArr = arr;
+let noRepeat = [ ...new Set(numsArr) ]
+
+console.log(noRepeat);
+
+let uniqs = numsArr.filter(function(item, index, self) {
+  return self.indexOf(item) === index;
+  // Esto crea un nuevo array y compara los valores del array original contra los del nuevo 
+})
+console.log(uniqs);
+}
+
+repeatNums(["x", 10, "x", 2, "10", 10, true, true]);
+/* 26) Programa una función que dado un arreglo de números obtenga el promedio, 
+pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5. */
+console.log('=== Ejercicio 26 ===');
+
+const arrAverage = (arr = undefined) => {
+
+  if (arr === undefined) return console.warn('No has ingresado nada');
+   if (!(arr instanceof Array)) return console.warn('El valor ingresado no es un array');
+   if (arr.length === 0) return console.warn('El arreglo está vacío');
+
+   for (let num of arr){
+     if (typeof num !== 'number') return console.warn(`El valor ${num} No es un numero`);
+   }
+
+  const numsArr = arr;
+  let sum = 0;
+
+  numsArr.forEach (function(el){
+    sum += el; // sum = sum + el;
+  });
+  console.log(sum);
+
+  let prom = sum / numsArr.length
+  console.log(prom)
+}
+
+arrAverage([9,8,7,6,5,4,3,2,1,0]);
+
+
+/* Solución profe */
+
+const promedio = (arr = unefined) => {
+
+  if (arr === undefined) return console.warn('No has ingresado nada');
+   if (!(arr instanceof Array)) return console.warn('El valor ingresado no es un array');
+   if (arr.length === 0) return console.warn('El arreglo está vacío');
+
+   for (let num of arr){
+     if (typeof num !== 'number') return console.warn(`El valor ${num} No es un numero`);
+   }
+
+   return console.log(
+     //arr.reduce
+   )
+
+}
